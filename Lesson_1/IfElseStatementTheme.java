aab3cf92d61f65e78e4f7a89e93aee9a09bae2fe
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-04-13 19:26:37 +0300

    add file IfElseStatementTheme

---
+++ Lesson_1/IfElseStatementTheme.java
@@ -0,0 +1,19 @@
+package Lesson_1;
+
+public class IfElseStatementTheme {
+
+  public static void main(String[] args) {
+    //1. Перевод псевдокода на язык Java
+    System.out.println("\n1. Перевод псевдокода на язык Java\n");
+
+    //2. Поиск максимального и минимального числа
+    System.out.println("\n2. Поиск максимального и минимального числа\n");
+    int srcNum1 = 235;
+    int srcNum2 = 10;
+    if (srcNum1 > srcNum2) {
+      System.out.println("\nЧисло " + srcNum1 + " больше " + srcNum2 + " \n");
+    } else {
+      System.out.println("\nЧисло " + srcNum2 + " больше " + srcNum1 + " \n");
+    }
+  } 
+}
