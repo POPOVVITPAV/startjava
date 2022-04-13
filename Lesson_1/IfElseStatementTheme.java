7854dbeabb1014791924c99fd14fa0749c945db6
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-04-13 20:36:47 +0300

    add some part

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -7,13 +7,31 @@ public class IfElseStatementTheme {
     System.out.println("\n1. Перевод псевдокода на язык Java\n");
 
     //2. Поиск максимального и минимального числа
-    System.out.println("\n2. Поиск максимального и минимального числа\n");
+    System.out.println("\n2. Поиск максимального и минимального числа");
     int srcNum1 = 235;
     int srcNum2 = 10;
     if (srcNum1 > srcNum2) {
-      System.out.println("\nЧисло " + srcNum1 + " больше " + srcNum2 + " \n");
+        System.out.println("\nЧисло " + srcNum1 + " больше " + srcNum2);
     } else {
-      System.out.println("\nЧисло " + srcNum2 + " больше " + srcNum1 + " \n");
+        System.out.println("\nЧисло " + srcNum2 + " больше " + srcNum1);
+    }
+
+    //3. Работа с числом
+    System.out.println("\n3. Работа с числом");
+    int someNumber = 5;
+    if (someNumber != 0) {
+        if (someNumber % 2 == 0) {
+            System.out.println("\nЧисло являеться четным");
+        } else {
+            System.out.println("\nЧисло являеться нечетным");
+        }
+        if (someNumber < 0 ) {
+          System.out.println("Число являеться отрицательным");
+        } else {
+          System.out.println("Число являеться положительным");
+        }
+    } else {
+        System.out.println("\nЧисло являеться нулем");
     }
   } 
 }
