30434f89a8bedd20968b40dece57389551d24b1d
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-04-27 21:34:07 +0300

    add file CyclesTheme.java

---
+++ Lesson_1/CyclesTheme.java
@@ -0,0 +1,12 @@
+package Lesson_1;
+
+public class CyclesTheme {
+
+    public static void main(String[] args) {
+        //1. Подсчет суммы четных и нечетных чисел
+        do {
+
+        } while (a <= 21);
+    }
+  
+}
