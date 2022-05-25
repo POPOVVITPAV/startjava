ac23f0f520f661f53b0a71ebd98a70cfe746dd22
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-05-25 21:09:12 +0300

    add file calculator.java

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        char sign = '+';
+        int result = 1; 
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '%') {
+            result = num1 % num2;
+        } else if (sign == '^') {
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+        }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
