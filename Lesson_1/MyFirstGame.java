f462c23809182c33276160dda1085c9f96db8e80
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-05-26 19:35:03 +0300

    new version

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -3,18 +3,17 @@ package Lesson_1;
 public class MyFirstGame {
     public static void main(String[] args) {
       int numOfComputer = 10;
-      boolean isFlage = true;
-      int numOfUser =1;
-      int a = 41;
-      int c = 1119;
-      int m = 111;
-      while (isFlage) {
-          numOfUser = (a * numOfUser + c) % m;
-          System.out.println(numOfUser);
-          if (numOfComputer == numOfUser) {
-              System.out.println("You win! Num is " + numOfUser);
-              isFlage = false;
-          }
+     // boolean isFlage = true;
+      int numOfUser = 0;
+      while (numOfUser != numOfComputer) {
+              numOfUser = (numOfUser * 2 + 1) % 11;  
+              numOfUser++;
+          if (numOfComputer < numOfUser) {
+              System.out.printf("\nчисло %d больше того, что загадал компьютер", numOfUser);
+          } else if (numOfComputer > numOfUser) {
+            System.out.printf("\nчисло %d меньше того, что загадал компьютер", numOfUser);
+        }
       }
+      System.out.println("\nYou win! Num is " + numOfUser);
     }
 }
