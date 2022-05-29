78f5235bdd36afdcfa811fd04593107934d56a41
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-05-29 17:58:28 +0300

    one a moryanother algorithm

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -3,17 +3,16 @@ package Lesson_1;
 public class MyFirstGame {
     public static void main(String[] args) {
       int numOfComputer = 10;
-     // boolean isFlage = true;
-      int numOfUser = 0;
-      while (numOfUser != numOfComputer) {
-              numOfUser = (numOfUser * 2 + 1) % 11;  
-              numOfUser++;
-          if (numOfComputer < numOfUser) {
-              System.out.printf("\nчисло %d больше того, что загадал компьютер", numOfUser);
-          } else if (numOfComputer > numOfUser) {
-            System.out.printf("\nчисло %d меньше того, что загадал компьютер", numOfUser);
-        }
-      }
-      System.out.println("\nYou win! Num is " + numOfUser);
+      int numOfPlayer = (2 * 1 + 3) % 9;
+      while (numOfPlayer != numOfComputer) {
+          if (numOfComputer < numOfPlayer) {
+              System.out.printf("\nчисло %d больше того, что загадал компьютер", numOfPlayer);
+              numOfPlayer++;
+          } else if (numOfComputer > numOfPlayer) {
+              System.out.printf("\nчисло %d меньше того, что загадал компьютер", numOfPlayer);
+              numOfPlayer--;
+          }
+     }
+      System.out.println("\nYou win! Num is " + numOfPlayer);
     }
 }
