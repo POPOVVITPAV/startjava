e6a96fef841d2f3c4bd7003ef707871c1a0cf909
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-06-05 19:32:05 +0300

    add class Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,26 @@
+package Lesson_2;
+
+public class Person {
+  
+    String gender = "male"; 
+    String name = "Ivan";
+    float height = 1.7f; 
+    float weight = 70; 
+    int age = 33;
+
+    void walk() {
+        System.out.println("I'm walking");
+    }
+    String sit() {
+        return "I'm sitting on a bench";
+    } 
+    void run() {
+        System.out.println("I'm running");
+    } 
+    boolean talk() {
+        return true;
+    } 
+    String learnJava() {
+        return "I'm learning java";
+    }
+}
