1f10315d123f14cff183eaff3ad645ec5d727792
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-06-08 21:34:58 +0300

    end calc

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -7,18 +7,34 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calc = new Calculator();
-        System.out.print("Введите первое число: ");
-        int num1 = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Введите знак математической операции: ");
-        char sign = sc.nextLine().charAt(0);
-        System.out.print("Введите второе число: ");
-        int num2 = sc.nextInt();
-
-        calc.setNum1(num1);
-        calc.setSign(sign);
-        calc.setNum2(num2);
-        calc.calculate();
+        do {
+          System.out.print("Введите первое число: ");
+          calc.setNum1(sc.nextInt());
+          sc.nextLine();
+          System.out.print("Введите знак математической операции: ");
+          calc.setSign(sc.nextLine().charAt(0));
+          System.out.print("Введите второе число: ");
+          calc.setNum2(sc.nextInt());
+          calc.calculate();
+          System.out.println("Хотите продолжить вычисления? [yes/no]");
+          sc.nextLine();
+          String answer = sc.nextLine();
+              if (!answer.equals("no") && !answer.equals("yes")) {
+                  while (true) {
+                    System.out.println("Хотите продолжить вычисления? [yes/no]");
+                    answer = sc.nextLine();
+                    if (!answer.equals("no") && !answer.equals("yes")) {
+                        continue;
+                    } else {
+                        break;
+                    }
+                  }
+              } else if (answer.equals("yes")) {
+                continue;
+              } else if (answer.equals("no")) {
+                break;
+              } 
+        } while (true);
         sc.close(); 
     }
 }
