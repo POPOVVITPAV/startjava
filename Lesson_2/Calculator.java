681e1dc532bf52e3c8edbef7c0ba41278e625eb3
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-06-10 22:18:06 +0300

    delet getters

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -6,24 +6,12 @@ public class Calculator {
     private int num2;
     private char sign;
 
-    public int getNum1() {
-      return num1;
-    }
-
     public void setNum1(int num1) {
       this.num1 = num1;
     }
 
-    public int getNum2() {
-      return num2;
-    }
-
     public void setNum2(int num2) {
       this.num2 = num2;
-    }
-
-    public char getSign() {
-      return sign;
     }
 
     public void setSign(char sign) {
@@ -32,31 +20,31 @@ public class Calculator {
 
     public void calculate() {
         int result = 1;
-            switch (sign) {
-                case '+': 
-                    result = num1 + num2;
-                  break;
-                case '-': 
-                    result = num1 + num2;
-                  break;
-                case '*': 
-                    result = num1 + num2;
-                  break;
-                case '/': 
-                    result = num1 + num2;
-                  break;
-                case '%': 
-                    result = num1 + num2;
-                  break;
-                case '^': 
-                    for (int i = 0; i < num2; i++) {
-                        result *= num1;
-                    }
-                  break;
-                default:
-                    System.out.println("Неизвестная операция");
-                  break;
-            }
+        switch (sign) {
+            case '+': 
+                result = num1 + num2;
+              break;
+            case '-': 
+                result = num1 + num2;
+              break;
+            case '*': 
+                result = num1 + num2;
+              break;
+            case '/': 
+                result = num1 + num2;
+              break;
+            case '%': 
+                result = num1 + num2;
+              break;
+            case '^': 
+                for (int i = 0; i < num2; i++) {
+                    result *= num1;
+                }
+              break;
+            default:
+              System.out.println("Неизвестная операция");
+              break;
+        }
         System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -7,34 +7,23 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calc = new Calculator();
-        do {
-          System.out.print("Введите первое число: ");
-          calc.setNum1(sc.nextInt());
-          sc.nextLine();
-          System.out.print("Введите знак математической операции: ");
-          calc.setSign(sc.nextLine().charAt(0));
-          System.out.print("Введите второе число: ");
-          calc.setNum2(sc.nextInt());
-          calc.calculate();
-          System.out.println("Хотите продолжить вычисления? [yes/no]");
-          sc.nextLine();
-          String answer = sc.nextLine();
-              if (!answer.equals("no") && !answer.equals("yes")) {
-                  while (true) {
-                    System.out.println("Хотите продолжить вычисления? [yes/no]");
-                    answer = sc.nextLine();
-                    if (!answer.equals("no") && !answer.equals("yes")) {
-                        continue;
-                    } else {
-                        break;
-                    }
-                  }
-              } else if (answer.equals("yes")) {
-                continue;
-              } else if (answer.equals("no")) {
-                break;
-              } 
-        } while (true);
-        sc.close(); 
-    }
-}
+            System.out.print("Введите первое число: ");
+            calc.setNum1(sc.nextInt());
+            sc.nextLine();
+            System.out.print("Введите знак математической операции: ");
+            calc.setSign(sc.nextLine().charAt(0));
+            System.out.print("Введите второе число: ");
+            calc.setNum2(sc.nextInt());
+            calc.calculate();
+            while (true) {
+              System.out.println("Хотите продолжить вычисления? [yes/no]");
+              sc.nextLine();
+              String answer = sc.nextLine();
+              
+            }
+             
+
+        sc.close();
+        } 
+  }
+       
