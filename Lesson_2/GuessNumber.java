89c6529e214c81ac8d3878270ecb0d8fdfd7a24c
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-06-20 21:46:02 +0300

    write some code

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,5 +1,22 @@
 package Lesson_2;
 
 public class GuessNumber {
-  
+   
+    Player player;
+
+    public void startGame() {
+        player = new Player();
+
+        int targetNumber = (int) (Math.random() * 100);
+        System.out.println("Компьютер загадал число от 0 до 100");
+
+        while (true) {
+            if (player.getNum())
+
+
+
+        }
+
+
+    }
 }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -1,5 +1,31 @@
 package Lesson_2;
 
+import java.net.Socket;
+import java.util.Scanner;
+
 public class GuessNumberTest {
-  
+    public static void main(String[] args) {
+        GuessNumber guessNumber = new GuessNumber();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Первый игрок введите свое имя");
+        String nameFirstPlayer = sc.nextLine();
+        System.out.println("Второй игрок введите свое имя");
+        String nameSecondPlayer = sc.nextLine();
+        Player player1 = new Player(nameFirstPlayer);
+        Player player2 = new Player(nameSecondPlayer);
+        String answer = "yes";
+        while (answer.equals("yes")) {
+            System.out.printf("%s введите число от 0 до 100", player1.getName());
+            System.out.printf("%s введите число от 0 до 100", player2.getName());
+            
+            do {
+                System.out.println("Хотите продолжить игру? [yes/no]:");
+                answer = sc.nextLine();    
+              } while (!(answer.equals("yes") || answer.equals("no")));
+        }
+        sc.close();
+
+    }
+    
+
 }
