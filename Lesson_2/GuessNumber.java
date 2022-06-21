1aa3575e8a488f7c746e9008734f7855f97ede9b
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-06-21 21:50:45 +0300

    write code for GuessNumber

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -3,20 +3,20 @@ package Lesson_2;
 public class GuessNumber {
    
     Player player;
-
-    public void startGame() {
+    
+    public boolean startGame() {
         player = new Player();
-
         int targetNumber = (int) (Math.random() * 100);
-        System.out.println("Компьютер загадал число от 0 до 100");
-
-        while (true) {
-            if (player.getNum())
-
-
-
+        System.out.println("Компьютер загадал число от 0 до 100" + targetNumber);
+        if (targetNumber == player.getNum()){
+            System.out.printf("\n Победу одержал %s", player.getName());
+            return true;
+        } else if (player.getNum() > targetNumber) {
+            System.out.printf("\nчисло %d больше того, что загадал компьютер", player.getNum());
+            return false;
+        } else {
+            System.out.printf("\nчисло %d меньше того, что загадал компьютер", player.getNum());
+            return false;
         }
-
-
     }
 }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -1,25 +1,31 @@
 package Lesson_2;
 
-import java.net.Socket;
 import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
         GuessNumber guessNumber = new GuessNumber();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Первый игрок введите свое имя");
+        System.out.printf("\nПервый игрок введите свое имя ");
         String nameFirstPlayer = sc.nextLine();
-        System.out.println("Второй игрок введите свое имя");
+        System.out.printf("\nВторой игрок введите свое имя ");
         String nameSecondPlayer = sc.nextLine();
+        sc.nextLine();
         Player player1 = new Player(nameFirstPlayer);
         Player player2 = new Player(nameSecondPlayer);
         String answer = "yes";
         while (answer.equals("yes")) {
-            System.out.printf("%s введите число от 0 до 100", player1.getName());
-            System.out.printf("%s введите число от 0 до 100", player2.getName());
-            
+            guessNumber.startGame();
+            while (guessNumber.startGame() != true) {
+                System.out.printf("\n%s введите число от 0 до 100 ", player1.getName());
+                player1.setNum(sc.nextInt());
+                sc.nextLine();
+                System.out.printf("\n%s введите число от 0 до 100 ", player2.getName());
+                player2.setNum(sc.nextInt());
+            }
+            sc.nextLine();
             do {
-                System.out.println("Хотите продолжить игру? [yes/no]:");
+                System.out.printf("Хотите продолжить игру? [yes/no12]:");
                 answer = sc.nextLine();    
               } while (!(answer.equals("yes") || answer.equals("no")));
         }
