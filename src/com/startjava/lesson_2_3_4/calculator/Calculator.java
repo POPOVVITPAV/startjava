33437a347a967a2a22dba666ecc751042be9c01b
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-08-14 22:05:53 +0300

    First part

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -7,41 +7,41 @@ public class Calculator {
     private char sign;
 
     public void setNum1(int num1) {
-      this.num1 = num1;
+        this.num1 = num1;
     }
 
     public void setNum2(int num2) {
-      this.num2 = num2;
+        this.num2 = num2;
     }
 
     public void setSign(char sign) {
-      this.sign = sign;
+        this.sign = sign;
+    }
+
+    public int getNum1() { return num1; }
+
+    public int getNum2() { return num2; }
+
+    public char getSign() { return sign; }
+
+    public void splitArray(String str) {
+        String[] strArray = str.split(" ");
+        setNum1(Integer.parseInt(strArray[0]));
+        setSign(strArray[1].charAt(0));
+        setNum2(Integer.parseInt(strArray[2]));
     }
 
     public int calculate() {
-        int result = 0;
+        //int result = 0;
         switch (sign) {
-            case '+': 
-                result = num1 + num2;
-                break;
-            case '-': 
-                result = num1 - num2;
-                break;
-            case '*': 
-                result = num1 * num2;
-                break;
-            case '/': 
-                result = num1 / num2;
-                break;
-            case '%': 
-                result = num1 % num2;
-                break;
-            case '^': 
-                result = (int) Math.pow(num1, num2);
-                break;
-            default:
-                System.out.println("Неизвестная операция ");
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            case '*': return num1 * num2;
+            case '/': return num1 / num2;
+            case '%': return num1 % num2;
+            case '^': return (int) Math.pow(num1, num2);
+            default: System.out.println("Неизвестная операция ");
         }
-        return result;
+        return 0;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -10,12 +10,8 @@ public class CalculatorTest {
         String answer = "yes";
         while (answer.equals("yes")) {
             System.out.print("Введите математическое выражение отделяя числа от символа пробелом: ");
-            String str = sc.nextLine();
-            String[] strArray = str.split(" ");
-            calc.setNum1(Integer.parseInt(strArray[0]));
-            calc.setSign(strArray[1].charAt(0));
-            calc.setNum2(Integer.parseInt(strArray[2]));
-            System.out.printf("Результат: %s %s %s = %d\n", strArray[0], strArray[1], strArray[2], calc.calculate());
+            calc.splitArray(sc.nextLine());
+            System.out.printf("%d %c %d = %d\n", calc.getNum1(), calc.getSign(), calc.getNum2(), calc.calculate());
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]"); 
                 answer = sc.nextLine();
