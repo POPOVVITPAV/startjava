ec6a955dadcc6e0b60a3ce3488ee1170b64a6e77
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-08-15 19:05:01 +0300

    new variant

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -6,23 +6,11 @@ public class Calculator {
     private int num2;
     private char sign;
 
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
-    }
-
-    public void setSign(char sign) {
-        this.sign = sign;
-    }
-
     public int calculate(String str) {
         String[] operands = str.split(" ");
-        setNum1(Integer.parseInt(operands[0]));
-        setSign(operands[1].charAt(0));
-        setNum2(Integer.parseInt(operands[2]));
+        num1 = Integer.parseInt(operands[0]);
+        sign = operands[1].charAt(0);
+        num2 = Integer.parseInt(operands[2]);
         switch (sign) {
             case '+': return num1 + num2;
             case '-': return num1 - num2;
