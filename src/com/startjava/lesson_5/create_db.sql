0328a380ed37d399fde91ba1550c2276341dd59a
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-11-15 21:38:44 +0300

    Added queries and removed spaces

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE Jaegers;
 \c Jaegers;
 CREATE TABLE jaegers (
-                      id serial primary key,
-                      modelName text not null,
-                      mark character(8) not null,
-                      height double precision not null,
-                      weight double precision not null,
-                      status text,
-                      origin text,
-                      kaijuKill integer not null
-                      );
+    id serial primary key,
+    modelName text not null,
+    mark character(8) not null,
+    height double precision not null,
+    weight double precision not null,
+    status text,
+    origin text,
+    kaijuKill integer not null
+);
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,8 +1,10 @@
-SELECT * FROM jaegers;
-SELECT * FROM jaegers WHERE origin = 'Destroyed';
-SELECT * FROM jaegers WHERE mark = 'Mark - 2' AND mark = 'Mark - 5'
-SELECT modelName FROM jaegers ORDER BY mark;
-SELECT modelName FROM jaegers WHERE height >= '80';
-SELECT MAX(kaijuKill) FROM jaegers;
-SELECT AVG(weight) FROM jaegers;
-DELETE modelName FROM jaegers WHERE status = 'Destroyeded';
+SELECT * FROM jaegers;                                                           \выведите всю таблицу
+SELECT * FROM jaegers WHERE origin = 'Destroyed';                                \отобразите только уничтоженных роботов
+SELECT * FROM jaegers WHERE mark IN ('Mark - 2', 'Mark - 4');                    \отобразите роботов нескольких серий: Mark-1 и Mark-4
+SELECT * FROM jaegers WHERE mark NOT IN ('Mark - 2', 'Mark - 4');                \отобразите всех роботов, кроме Mark-1 и Mark-4
+SELECT modelName FROM jaegers ORDER BY mark;                                     \отсортируйте таблицу по убыванию по столбцу mark
+SELECT modelName FROM jaegers WHERE height >= '80';                              \отобразите информацию о самом старом(высоком) роботе
+SELECT MAX(kaijuKill) FROM jaegers;                                              \отобразите роботов, которые уничтожили больше всех kaiju
+SELECT AVG(weight) FROM jaegers;                                                 \отобразите средний вес роботов
+UPDATE jaegers SET kaijuKill = kaijuKill + 1 WHERE status = 'Not destroyed';     \увеличьте на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены
+DELETE modelName FROM jaegers WHERE status = 'Destroyeded';                      \удалите уничтоженных роботов
